4ca0efa7f565b52eb20e64793d0b3437d053b11b
Ruben Urbina <rubenurbina69@gmail.com>
2026-04-21 21:42:17 -0600

    conexion a postgres

--- app/database.py
+++ app/database.py
@@ -1,12 +1,23 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./huevos_alak.db"
+# 🔥 Detecta si estamos en Render o en local
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+# 👉 Si NO existe (trabajando en tu PC)
+if not DATABASE_URL:
+    DATABASE_URL = "sqlite:///./huevos_alak.db"
+
+# 👉 Si es postgres (Render)
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+# Configuración especial solo para SQLite
+connect_args = {"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
